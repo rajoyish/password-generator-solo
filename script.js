8c39bef7f7aa7ded3712eebff39b891f75f3b4a4
Rajesh Budhathoki <rajoyish@gmail.com>
2025-05-04 08:24:00 +0545

    Finish Password Generator Solo

---
+++ script.js
@@ -0,0 +1,114 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
+
+let firstPasswordEl = document.querySelector('#first-password');
+let secondPasswordEl = document.querySelector('#second-password');
+let passwordBtn = document.querySelector('.random-pass--btn');
+
+function generatePassword(length = 15) {
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+firstPasswordEl.value = '';
+secondPasswordEl.value = '';
+
+passwordBtn.addEventListener('click', () => {
+  firstPasswordEl.value = generatePassword();
+  secondPasswordEl.value = generatePassword();
+});
